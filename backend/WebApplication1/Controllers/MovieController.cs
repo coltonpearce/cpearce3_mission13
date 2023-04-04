a0726c144b8217f9da601f5672dfaf8be83a1bf5
coltonpearce <113003120+coltonpearce@users.noreply.github.com>
2023-04-04 16:01:22 -0600

    cpearce3_mission14

---
+++ backend/WebApplication1/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Controllers.Data;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+        public MovieController(MovieDbContext temp)
+        {
+            context = temp;
+        }
+        public IEnumerable <Movie> Get()
+        {
+            return context.Movies.ToArray()
+                .Where(m => m.Edited == "Yes")
+                .OrderBy(m => m.Title);
+        }
+    }
+}
